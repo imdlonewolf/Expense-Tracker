4aed8109098ccb189be1d0ea814952fce1b5a062
Nirjhar Das <76589787+imdlonewolf@users.noreply.github.com>
2026-08-26 21:35:35 +0530

    Update CORS policy to allow new frontend origin

--- backend/Web_Api/Program.cs
+++ backend/Web_Api/Program.cs
@@ -35,7 +35,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontendCloud", policy =>
     {
-        policy.WithOrigins("https://expense-tracker-one-chi-42.vercel.app")
+        policy.WithOrigins("expense-tracker-git-push-to-live-nir14.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
